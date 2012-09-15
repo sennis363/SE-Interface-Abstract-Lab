4a43b790f366837a7d68c00ada1c370730e9dbfe
Michael <Michael@Sean-PC>
2012-09-15 16:48:42 -0500

    Starting out in lab 1.

---
+++ src/lab1/SoftwareDevelopmentCourses.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+
+/**
+ *
+ * @author Michael
+ */
+public abstract class SoftwareDevelopmentCourses {
+    String studentName;
+    String teacherName;
+}
---
+++ src/lab1/Startup.java
@@ -0,0 +1,7 @@
+package lab1;
+
+public class Startup {
+    public static void main(String[] args) {
+        
+    }
+}
