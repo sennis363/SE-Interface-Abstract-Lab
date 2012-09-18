adc19fe75de5581ab585b07a3e68d76b77f0e0ef
Michael <Michael@Sean-PC>
2012-09-18 17:24:35 -0500

    Made all methods final, removed redundant code in sub-classes.

--- src/lab1/Startup.java
+++ src/lab1/Startup.java
@@ -3,13 +3,12 @@ package lab1;
 public class Startup {
     public static void main(String[] args) {
         AdvancedJavaCourse advJava1 = new AdvancedJavaCourse();
+        SoftwareDevelopmentCourse test = new AdvancedJavaCourse();
         IntroJavaCourse intJava1 = new IntroJavaCourse();
         IntroToProgrammingCourse intProg1 = new IntroToProgrammingCourse();
         
         advJava1.setCourseName("Advanced Java");
         advJava1.setCourseNumber("12345");
-        advJava1.setTeacherName("Bob McBob");
-        advJava1.setStudentName("Larry Wing");
         
         intJava1.setCourseName("Intro to Java");
         intJava1.setCourseNumber("55455");
@@ -17,16 +16,18 @@ public class Startup {
         intProg1.setCourseName("Intro to Programming");
         intProg1.setCourseNumber("99999");
         
+        test.setPrerequisites("BLARG");
+        
         System.out.println("Course : " + advJava1.getCourseName());
         System.out.println("Course # : " + advJava1.getCourseNumber());
-        System.out.println("Teacher : " + advJava1.getTeacherName());
-        System.out.println("Student : " + advJava1.getStudentName());
         System.out.println("");
         System.out.println("Course : " + intJava1.getCourseName());
         System.out.println("Course # : " + intJava1.getCourseNumber());
         System.out.println("");
         System.out.println("Course : " + intProg1.getCourseName());
         System.out.println("Course # : " + intProg1.getCourseNumber());
+        
+        System.out.println("TEST : " + test.getPrerequisites());
         /*I like how it is harder for the program to fail due to the fact that
          * all abstract methods must be overidden. The one thing I don't like
          * about this is having to actually overide all of the abstract methods,
